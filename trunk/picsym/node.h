600ebfd4ae45fbe68ede8b67a3d7bd34c10d0648
schukin <schukin@5d4fd40b-98bb-4374-885e-08477ff531e6>
2014-02-25 09:16:54 +0000

    Some fixes

---
+++ trunk/picsym/node.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace picsym {
+
+class Node {
+};
+
+}
+
---
+++ trunk/picviz.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class PICVisualizer {
+public:
+    PICVisualizer() {}
+    ~PICVisualizer() {}
+
+    void draw();
+};
+
